cdfe92fb6149468e4d9aa68142eb760b1c61ca37
Ryan <ryan@mail.com>
2024-03-05 12:42:02 +0000

    Publicado la creacion de las migraciones

---
+++ database/migrations/2024_03_05_123039_empresa.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('cif');
+            $table->string('telefono');
+            $table->string('email');
+            $table->string('iban');
+            $table->string('direccion');
+            $table->string('municipio');
+            $table->string('contraseña');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empresa');
+    }
+};
