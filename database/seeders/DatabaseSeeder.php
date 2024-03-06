4062f23006d9b0c1aa7837910d95c71a74010321
aitziS <aitzisolba@gmail.com>
2024-03-06 10:17:18 +0000

    seed clientes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         \App\Models\Empresa::factory(10)->create();
+        \App\Models\Cliente::factory(10)->create();
     }
 }
