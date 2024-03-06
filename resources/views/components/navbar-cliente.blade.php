274f97d91be397204376139ac32eb2fb022ff1e0
Ryan <ryan@mail.com>
2024-03-06 12:44:42 +0000

    making the logic for navbar

--- resources/views/components/navbar-cliente.blade.php
+++ resources/views/components/navbar-cliente.blade.php
@@ -1,17 +1,15 @@
 <nav>
+    <li><a href=""><img src="{{ asset('img/logo-negativo.svg') }}" class="nav-logo" alt=""></a></li>
     <ul>
-        <li><a href=""><img src="{{ asset('img/logo-negativo.svg') }}" alt="" style="background-color: blue;"></a></li>
-        @if (Route::has('login'))
-            @auth
-                <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
-            @else
-                <li><a href="{{ route('login') }}">Login</a></li>
+        @if (Route::has('home'))
+            <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+        @else
+            <li><a href="{{ route('login') }}">Login</a></li>
 
-                @if (Route::has('register'))
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                @endauth
+            @if (Route::has('register'))
+                <li><a href="{{ route('register') }}">Register</a></li>
             @endif
         @endif
-            <li><a href="/about">About</a></li>
+        <li><a href="/about">About</a></li>
     </ul>
 </nav>
